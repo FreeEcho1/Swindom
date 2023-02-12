87ff580715e067fc0a8858a36d54d858ce903583
FreeEcho <20364419+FreeEcho1@users.noreply.github.com>
2023-02-12 10:21:01 +0900

    Add files via upload

--- Swindom/Swindom/Properties/Resources.Designer.cs
+++ Swindom/Swindom/Properties/Resources.Designer.cs
@@ -61,6 +61,26 @@ namespace Swindom.Properties {
         }
         
         /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] AdditionDark {
+            get {
+                object obj = ResourceManager.GetObject("AdditionDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] AdditionWhite {
+            get {
+                object obj = ResourceManager.GetObject("AdditionWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   (アイコン) に類似した型 System.Drawing.Icon のローカライズされたリソースを検索します。
         /// </summary>
         internal static System.Drawing.Icon ApplicationIcon {
@@ -77,6 +97,286 @@ namespace Swindom.Properties {
             get {
                 object obj = ResourceManager.GetObject("ApplicationIconInvalid", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] CloseSettingsDark {
+            get {
+                object obj = ResourceManager.GetObject("CloseSettingsDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] CloseSettingsWhite {
+            get {
+                object obj = ResourceManager.GetObject("CloseSettingsWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] CopyDark {
+            get {
+                object obj = ResourceManager.GetObject("CopyDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] CopyWhite {
+            get {
+                object obj = ResourceManager.GetObject("CopyWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] DeleteDark {
+            get {
+                object obj = ResourceManager.GetObject("DeleteDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] DeleteWhite {
+            get {
+                object obj = ResourceManager.GetObject("DeleteWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] DownDark {
+            get {
+                object obj = ResourceManager.GetObject("DownDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] DownWhite {
+            get {
+                object obj = ResourceManager.GetObject("DownWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] HotkeyProcessingDark {
+            get {
+                object obj = ResourceManager.GetObject("HotkeyProcessingDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] HotkeyProcessingWhite {
+            get {
+                object obj = ResourceManager.GetObject("HotkeyProcessingWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] InformationDark {
+            get {
+                object obj = ResourceManager.GetObject("InformationDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] InformationWhite {
+            get {
+                object obj = ResourceManager.GetObject("InformationWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] LinkedChainDark {
+            get {
+                object obj = ResourceManager.GetObject("LinkedChainDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] LinkedChainWhite {
+            get {
+                object obj = ResourceManager.GetObject("LinkedChainWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] MagnetProcessingDark {
+            get {
+                object obj = ResourceManager.GetObject("MagnetProcessingDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] MagnetProcessingWhite {
+            get {
+                object obj = ResourceManager.GetObject("MagnetProcessingWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] ModifyDark {
+            get {
+                object obj = ResourceManager.GetObject("ModifyDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] ModifyWhite {
+            get {
+                object obj = ResourceManager.GetObject("ModifyWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] SettingsDark {
+            get {
+                object obj = ResourceManager.GetObject("SettingsDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] SettingsWhite {
+            get {
+                object obj = ResourceManager.GetObject("SettingsWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Drawing.Bitmap のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static System.Drawing.Bitmap SpecifyWindowProcessingDark {
+            get {
+                object obj = ResourceManager.GetObject("SpecifyWindowProcessingDark", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Drawing.Bitmap のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static System.Drawing.Bitmap SpecifyWindowProcessingWhite {
+            get {
+                object obj = ResourceManager.GetObject("SpecifyWindowProcessingWhite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] TargetDark {
+            get {
+                object obj = ResourceManager.GetObject("TargetDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] TargetWhite {
+            get {
+                object obj = ResourceManager.GetObject("TargetWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] UnattachedChainDark {
+            get {
+                object obj = ResourceManager.GetObject("UnattachedChainDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] UnattachedChainWhite {
+            get {
+                object obj = ResourceManager.GetObject("UnattachedChainWhite", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] UpDark {
+            get {
+                object obj = ResourceManager.GetObject("UpDark", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   型 System.Byte[] のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static byte[] UpWhite {
+            get {
+                object obj = ResourceManager.GetObject("UpWhite", resourceCulture);
+                return ((byte[])(obj));
             }
         }
     }
